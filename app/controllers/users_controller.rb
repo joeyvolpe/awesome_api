bc5dd77a55198ffd7e26bb88a91b780605a9d812
socialight21 <vcarloscruz@gmail.com>
2015-04-30 16:46:56 -0700

    adding basic functions to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,45 @@
 class UsersController < ApplicationController
 
-	def index
-		@users = User.all
-	end
+  def index
+    @users = User.all
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
+  def show
+    @user = User.find(params[:id])
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to user_path(@user)
-		else
-			render :new
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
 
-	def edit
-		@user = User.find(params[:id])
-	end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-	def update
-		@user = User.find(params[:id])
-		@user = User.update_attributes(user_params)
-	end
+  def update
+    @user = User.find(params[:id])
+    @user = User.update_attributes(user_params)
+  end
 
-	def destroy
-	end
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
 
-	private
-	def user_params
-		params.require(:user).permit(:name, :email, :password_digest)
-	end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest)
+  end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
- root 'static_pages#home'
+ root 'users#index'
  get 'contact' => 'static_pages#contact'
 
  get 'users' => 'users#index', as: :users
  get 'users/new' => 'users#new', as: :new_user
  post 'users' => 'users#create'
  get 'users/:id' => 'users#show', as: :user
+
+ delete 'users/:id' => 'users#destroy'
 
  
 end
