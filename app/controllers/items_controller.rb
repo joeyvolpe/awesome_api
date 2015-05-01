48710417a212986d4504f2badcc9ac8dcef4cfb7
Joey Volpe <thevolpe@gmail.com>
2015-04-30 18:28:15 -0700

    moving to feature branch...

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,43 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item = Item.update_attributes(item_params)
+
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to user_path(@item.user)
+  end
+
+  def item_params
+    params.require(:item).permit(:item_image, :item_name, :item_description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,18 @@ Rails.application.routes.draw do
  post 'users' => 'users#create'
  get 'users/:id' => 'users#show', as: :user
 
+
+ post "users/:id/items" => "items#create", as: :user_items
+
+
+ get 'items' => 'items#index'
+ get 'items/new' => 'items#new', as: :new_item
+ post 'items' => 'items#create', as: :item
+ get 'items/:id/edit' => "items#edit", as: :edit_item
+ patch 'items/:id' => 'items#update'
+ delete 'items/:id' => 'items#destroy'
+
+
  
+
 end
