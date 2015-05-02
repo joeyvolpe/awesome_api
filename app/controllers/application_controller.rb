aca41422fbabdf3708245376982dfd968ac4d81e
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-02 10:32:07 -0700

    Add authentication, functional link to current_user, login, logout in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < ActionController::Base
-
-
-  def austin
-    @austin
-  end
-
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
 
-  # Hi joey
-  
+  private
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:login][:email])
+		if user && user.authenticate(params[:login][:password])
+			#sets a cookie so our browser knows who we say we are
+			session[:user_id] = user.id.to_s
+			redirect_to root_path
+			#session = {:user_id => "4"}
+
+			# session = Hash.new
+			# {}
+
+			# session[:user_id] = "joey"
+			# {:user_id => "joey"}
+		else
+			redirect_to login_path
+		end
+	end
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to login_path
+	end
+end
+
