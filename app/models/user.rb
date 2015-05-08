4f7fc630ffad380ed643425de1085f03503fdaa5
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-08 15:15:26 -0700

    Semantic switch branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,9 @@
 class User < ActiveRecord::Base
   has_many :items, :dependent => :destroy
   has_many :trades, :dependent => :destroy
+  has_secure_password
   attr_reader :password
 
-  def password=(unencrypted_password)
-  	unless unencrypted_password.empty?
-  		@password = unencrypted_password
-  		self.password_digest = BCrypt::Password.create(unencrypted_password)
- 	  end
-  end	
-
-  def authenticate(unencrypted_password)
-  	if BCrypt::Password.new(self.password_digest) == unencrypted_password
-  		return self
-  	else 
-  		return false
-    end
-  end
-
   mount_uploader :image, ProfilePicUploader
-
+  
 end
