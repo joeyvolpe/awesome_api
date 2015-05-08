379e03b36a0fd5c732d1e9f6560449082dd4e80b
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-08 15:24:51 -0700

    commented out authentication and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,21 @@ class User < ActiveRecord::Base
   before_save :format_user_input
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-validates :password, presence: true, confirmation: true, length: { in: 6..20 }
-  def password=(unencrypted_password)
-  	unless unencrypted_password.empty?
-  		@password = unencrypted_password
-  		self.password_digest = BCrypt::Password.create(unencrypted_password)
- 	  end
-  end	
+  has_secure_password
+  # def password=(unencrypted_password)
+  # 	unless unencrypted_password.empty?
+  # 		@password = unencrypted_password
+  # 		self.password_digest = BCrypt::Password.create(unencrypted_password)
+ 	#   end
+  # end	
 
-  def authenticate(unencrypted_password)
-  	if BCrypt::Password.new(self.password_digest) == unencrypted_password
-  		return self
-  	else 
-  		return false
-    end
-  end
+  # def authenticate(unencrypted_password)
+  # 	if BCrypt::Password.new(self.password_digest) == unencrypted_password
+  # 		return self
+  # 	else 
+  # 		return false
+  #   end
+  # end
 
   mount_uploader :image, ProfilePicUploader
 
