9599bd088c20c24c8553c1ecc293c250f552c7ac
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-12 19:00:52 -0700

    minor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,10 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:login][:email])
 		if user && user.authenticate(params[:login][:password])
-			#sets a cookie so our browser knows who we say we are
 			session[:user_id] = user.id.to_s
 			redirect_to root_path
-			#session = {:user_id => "4"}
-
-			# session = Hash.new
-			# {}
-
-			# session[:user_id] = "joey"
-			# {:user_id => "joey"}
 		else
-		flash.now[:error] = "Your email address or password are incorrect." 
+			flash.now[:error] = "Your email address or password are incorrect." 
 			redirect_to login_path
 		end
 	end
