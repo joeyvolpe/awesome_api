2d7efe96b20dbc4730b9a691b955786b215b4325
Joey Volpe <thevolpe@gmail.com>
2015-05-14 16:34:46 -0700

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = User.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+users = Item.create([{ item_name: 'Hoola Hoop', item_description: 'Circular tube you can spin around your waist for entertainment and what not.', user_id }, { name: 'Freddie', last_name: 'Mac', email: 'freddie@example.com' }])
