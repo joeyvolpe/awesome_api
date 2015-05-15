32f089a30e62d437d3953ebc13fde9f68fb21b75
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-14 18:48:42 -0700

    hope it works

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
 namespace :api do
 	resources :items, only: [:index, :show]
 	resources :users, only: [:index, :show]
-	resources :trades, only: [:index, :show]
+	resources :trades, only: [:index]
 end
 
 
