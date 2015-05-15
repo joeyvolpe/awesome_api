f0369de5558220ffbf5a7ccce109b28fcdae4c56
Joey Volpe <thevolpe@gmail.com>
2015-05-14 20:13:09 -0700

    restrict duplicate trades from one party, restrict other trades on an item once it's been traded away

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = Item.create([{ item_name: 'Hoola Hoop', item_description: 'Circular tube you can spin around your waist for entertainment and what not.', user_id }, { name: 'Freddie', last_name: 'Mac', email: 'freddie@example.com' }])
+users = Item.create([{ item_name: 'Hoola Hoop', item_description: 'Circular tube you can spin around your waist for entertainment and what not.', user_id: 1 }, { item_name: 'Rasta Hat', item_description: 'Jamaican hat. So awesome. So awesome.', user_id: 1 }, { item_name: 'Pickle', item_description: 'Brined in the finest of brines. From the anciet sea clay of Mesopotamia.', user_id: 2 }, { item_name: 'Noodle Roller', item_description: 'Used by Italians across the land. Or as a pool toy.', user_id: 2 }])
