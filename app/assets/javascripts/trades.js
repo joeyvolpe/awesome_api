3ef23ef019d4dbfaf40944059b9859df0261a83f
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-15 17:25:03 -0700

    Javascript onload

--- app/assets/javascripts/trades.js
+++ app/assets/javascripts/trades.js
@@ -1,6 +1,7 @@
-var API_BASE = '/api/items'
+var API_BASE = '/api/items';
 
-$(document).ready(function(){
+$(document).on('page:load', function(){
+
     var json = $.ajax({
         datatype: 'json',
         type: 'GET',
@@ -8,41 +9,49 @@ $(document).ready(function(){
         data: {}
     });
 
-    console.log(json);
+    // console.log(json);
+
+// Random function
+   function randomNumber(arr){
+   	 return arr[Math.floor(Math.random()*arr.length)];
+	};
+
+	var randNum = randomNumber(json);
+// End random function
 
     json.done(function(data){
         var i;
         for(i = 0; i < 4; i += 1){
-            console.log(data[i]);
             addItem($('#bottom'), data[i]);
         }
     });
     json.fail(function(data){
         $('#bottom').html("<h2>FAILED TO LOAD</h2>");
     });
+    json.done(function(data){
+	    var i;
+	    for(i = 3; i >= 0; i = i - 1){
+	      console.log(data[i]);
+	      addItem($('#top'), data[i]);
 
- json.done(function(data){
-   var i;
-   for(i = 0; i < 4; i += 1){
-     console.log(data[i]);
-     addItem($('#top'), data[i]);
-   }
- });
- json.fail(function(data){
-   $('#top').html("<h2>FAILED TO LOAD</h2>");
- });
+    }
+ 	 });
+ 	 json.fail(function(data){
+    $('#top').html("<h2>FAILED TO LOAD</h2>");
+ 	 });
 
     function addItem(jqElem, data){
         console.log(jqElem);
         console.log(data);
         var itemImage = data.item_image;
-   var userImage = data.user_image;
+   	  var userImage = data.user_image;
 
 
-        var listItem = "<div class='two wide column'>"; 
-        listItem += "<img src=' " + itemImage + "'></div>";
+        var listItem = "<div class='four wide column'>"; 
+        listItem += "<img class='ui medium image' src=' " + itemImage + "'></div>";
 
         jqElem.append(listItem);
         console.log(listItem);
     }
+
 });
--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,24 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :item_name, :item_description, :item_image, :user_fullname, :user_image 
-<<<<<<< HEAD
 
-	def user
-		user_id = object.user_id
-		User.find(user_id)
-	end
 
-	def user_fullname
-		"#{user.name} #{user.last_name}"
-	end
-
-	def user_image
-		user.image
-	end
-
-	def item_image
-		object.item_image.url
-	end
-=======
 
     def user
         user_id = object.user_id
@@ -36,6 +19,6 @@ class ItemSerializer < ActiveModel::Serializer
     def item_image
         object.item_image.url
     end
->>>>>>> development
+
   
 end
